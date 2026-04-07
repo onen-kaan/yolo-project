9edf39f20d9674babdc5c2aa1c7770c618dcbf55
onen-kaan <onen_kaan_tr+work@gmail.com>
2026-04-07 09:30:24 +0300

    add get video images

--- app/utilities/utils.py
+++ app/utilities/utils.py
@@ -1,6 +1,8 @@
 import yaml
 import os
 from typing import Any
+import numpy as np
+import cv2
 
 
 def getFromConfig(configPath: str, key: str) -> Any | None:
@@ -10,3 +12,22 @@ def getFromConfig(configPath: str, key: str) -> Any | None:
     with open(configPath, "r") as f:
         config: dict[str, Any] = yaml.safe_load(f)
         return config.get(key)
+
+
+def getVideoImages(videoPath: str, frameCount: int) -> list[np.ndarray]:
+
+    cap = cv2.VideoCapture(videoPath)
+    frames: list[np.ndarray] = []
+
+    totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for targetIdx in range(0, totalFrames, frameCount):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, targetIdx)
+
+        success, frame = cap.read()
+
+        if success:
+            frames.append(frame)
+
+    cap.release()
+    return frames
